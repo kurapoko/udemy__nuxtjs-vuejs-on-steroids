f11221621f7e175f85c6c97c67f300a84c175e52
kurapoko <kuraharamasahiro@gmail.com>
2020-04-08 08:51:57 +0900

    レイアウト変更・エラーページ作成

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <h1>An error occurred, we're sorry!</h1>
 </template>
 
 <style lang="stylus" scoped>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,21 +1,9 @@
 <template>
-  <div>
-    <input type="text" v-model="userId">
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Please enter an id and click the button.</p>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      userId: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userId)
-    }
-  }
+  layout: 'users'
 }
 </script>
