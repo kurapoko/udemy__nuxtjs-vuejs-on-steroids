d73a46f318bb94cd4726f1e326945882d5de6154
kurapoko <kuraharamasahiro@gmail.com>
2020-04-09 08:49:15 +0900

    ウェブフォント追加、ファイル準備

---
+++ pages/posts/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="posts-page">
+        <h1>This page is About</h1>
+        <p>aaaaaaaaaaaaaaaaaaaaa</p>
+    </div>
+</template>
+
+<style scoped>
+    .posts-page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
